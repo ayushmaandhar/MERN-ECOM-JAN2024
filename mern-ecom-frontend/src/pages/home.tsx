8d4c9568aa4b3dd932b71276ebe320b839d6764f
Ayushmaan Dhar <ayushmaandhar@gmail.com>
2024-01-04 12:28:12 +0530

    new: Product Card

--- mern-ecom-frontend/src/pages/home.tsx
+++ mern-ecom-frontend/src/pages/home.tsx
@@ -1,9 +1,31 @@
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 
 const Home = () => {
+
+  const addToCardHandler = () => {}
+
   return (
-    <div>
-      Home
+    <div className="home">
+      <section></section>
+      
+      <h1>
+        Latest Products
+        <Link to={'/search'} className="findmore">More</Link>
+      </h1>
+
+      <main>
+        <ProductCard
+          productId="asdsas"
+          name="MacBook"
+          price={4545}
+          stock={435}
+          handler={addToCardHandler}
+          photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg"
+        />
+      </main>
+
     </div>
   )
 }
