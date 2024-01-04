5ba302ba9d764e2c4d041ee0b194865573d50330
Ayushmaan Dhar <ayushmaandhar@gmail.com>
2024-01-04 21:29:57 +0530

    new: Login Page

--- mern-ecom-frontend/src/App.tsx
+++ mern-ecom-frontend/src/App.tsx
@@ -9,6 +9,7 @@ const Home = lazy(()=>import('./pages/home'));
 const Search = lazy(()=>import('./pages/search'));
 const Cart = lazy(()=>import('./pages/cart'));
 const Shipping = lazy(()=>import('./pages/shipping'));
+const Login = lazy(()=>import('./pages/login'));
 
 
 // Admin dashboard importing
@@ -40,6 +41,9 @@ const App = () => {
           <Route path='/' element={<Home/>}/>
           <Route path='/search' element={<Search/>}/>
           <Route path='/cart' element={<Cart/>}/>
+
+          {/* Not Logged In Route*/}
+          <Route path='/login' element={<Login/>}/>
 
           {/* Logged In User Routes */}
           <Route>
